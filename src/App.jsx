29bce49a2bbf27596afffdd2195851659b7b0eae
Rajat <rajat804@gmail.com>
2026-01-31 15:30:08 +0530

    app  js update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
+import Propertypage from "./pages/PropertyPage";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about-us" element={<AboutUs/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/properties" element={<Propertypage/>}></Route>
+        
       </Routes>
       <Footer />
 
