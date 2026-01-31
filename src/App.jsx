f94a0a538d0fff3f45d3ba495250f7769f31807e
Rajat <rajat804@gmail.com>
2026-01-31 10:47:36 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import {Routes, Route} from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
+
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about-us" element={<AboutUs/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+      </Routes>
       <Footer />
+
+      
     </>
   );
 }
