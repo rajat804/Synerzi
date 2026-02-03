4c275db75ca2d80b0dfd5dd406bd12a3ebb731c6
Rajat <rajat804@gmail.com>
2026-02-03 18:21:01 +0530

    app js upader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, useLocation} from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -9,12 +9,25 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import GoogleSuccess from "./pages/GoogleSuccess";
 import UserDashboard from "./pages/UserDashboard";
+import AdminRegister from "./pages/AdminRegister";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+
 
 function App() {
+  const location = useLocation();
+
+  // admin page header or footer hide
+  const hideHeaderandFooter = [
+    "/admin-login",
+    "/admin-register",
+    "/admin-dashboard"
+  ].includes(location.pathname);
+
   return (
     <>
 
-      <Header />
+      {!hideHeaderandFooter && <Header />}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about-us" element={<AboutUs/>}></Route>
@@ -24,8 +37,14 @@ function App() {
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/google-success" element={<GoogleSuccess/>}></Route>
         <Route path="/dashboard" element={<UserDashboard/>}></Route>
+
+
+        {/* admin  */}
+        <Route path="/admin-login" element={<AdminLogin/>}></Route>
+        <Route path="/admin-register" element={<AdminRegister/>}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard/>}></Route>
       </Routes>
-      <Footer />
+      {!hideHeaderandFooter && <Footer />}
 
       
     </>
