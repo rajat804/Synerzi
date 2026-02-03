19578004d150b032957cead7474ef69c60a97c6c
Rajat <rajat804@gmail.com>
2026-02-03 14:12:47 +0530

    new more add backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,14 +10,14 @@ const Login = () => {
   const [error, setError] = useState("");
 
   const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
-  const googleLogin = () => window.location.href = "https://synerzi.vercel.app/api/auth/google";
+  const googleLogin = () => window.location.href = "https://synerzi-backend.vercel.app/api/auth/google";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true); setError("");
 
     try {
-      const response = await fetch("https://synerzi.vercel.app/api/auth/login", {
+      const response = await fetch("https://synerzi-backend.vercel.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -70,6 +70,6 @@ const Login = () => {
       </div>
     </section>
   );
-};
+};  
 
 export default Login;
