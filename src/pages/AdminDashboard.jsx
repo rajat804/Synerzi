70ff1fb8f0962f80295f1f63dd2be2462666cd99
Rajat <rajat804@gmail.com>
2026-02-07 10:37:44 +0530

    admin dashboard update space

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -92,8 +92,6 @@ export default function AdminDashboard() {
               >
                 📋 Show Listings
               </Link>
-
-              
             </div>
           </div>
         </main>
