80d906ec63626da5c5f9a1ebf341e55f7a81145e
Rajat <rajat804@gmail.com>
2026-02-09 17:45:01 +0530

    update property area

--- src/components/PropertyArea.jsx
+++ src/components/PropertyArea.jsx
@@ -75,7 +75,7 @@ const PropertyArea = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {propertyAreas.map((area, index) => (
               <div
                 key={index}
