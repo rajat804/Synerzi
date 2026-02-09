f563fc8ef338e9cd627fc47c599426dd096346b2
Rajat <rajat804@gmail.com>
2026-02-09 22:02:10 +0530

    p is updated

--- src/pages/CategoryProperties.jsx
+++ src/pages/CategoryProperties.jsx
@@ -33,7 +33,7 @@ const CategoryProperties = () => {
         <p>No properties found</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {properties.map((property) => (
+          {properties.map((p) => (
             <div
               key={p._id}
               className="bg-white rounded-xl shadow hover:shadow-lg"
