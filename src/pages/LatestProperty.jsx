855e51d909d4c740a217847b54e5fe131e300957
Rajat <rajat804@gmail.com>
2026-02-10 12:11:17 +0530

    add

--- src/pages/LatestProperty.jsx
+++ src/pages/LatestProperty.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function LatestProperty() {
   const [properties, setProperties] = useState([]);
