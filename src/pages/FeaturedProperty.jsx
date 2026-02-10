c0691a147c83931b1c744b16b96e26c1c42d0c41
Rajat <rajat804@gmail.com>
2026-02-10 12:11:09 +0530

    add

--- src/pages/FeaturedProperty.jsx
+++ src/pages/FeaturedProperty.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 export default function FeaturedProperty() {
   const [properties, setProperties] = useState([]);
