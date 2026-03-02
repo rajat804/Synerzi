77fb4ddaab273cc83b9ad9ae71e9f10bab1177dc
Rajat <rajat804@gmail.com>
2026-03-02 12:46:05 +0530

    main jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./components/AuthComponent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
   <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
+  </AuthProvider>
 );
